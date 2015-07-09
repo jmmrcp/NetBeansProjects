88d37c31809681da577ee34eba9cec85cd048aec
jmmrcp <jmmrcp@gmail.com>
2015-07-09 20:17:50 +0200

    Cambios GDX
    Assest añadidos

--- GLX/desktop/src/es/computerwizards/game/desktop/DesktopLauncher.java
+++ GLX/desktop/src/es/computerwizards/game/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import es.computerwizards.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+                config.width = 480;
+                config.height = 320;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
